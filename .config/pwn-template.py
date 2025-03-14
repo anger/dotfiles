0d153fff60919633af467d2c68c328eee458c898
jax <48845671+anger@users.noreply.github.com>
2025-03-13 21:15:56 -0400

    Update pwn-template.py

--- .config/pwn-template.py
+++ .config/pwn-template.py
@@ -5,32 +5,26 @@ from pwn import *
 {bindings}
 
 elf = ({bin_name})
-
+context.binary = elf
 context.terminal = ["tmux", "split-window", "-h"]
 context.log_level = "DEBUG"
-context.binary = elf
-
 gdb_cmds = ["c"]
 
-def sla(x : bytes | str, y : bytes | str): p.sendlineafter(x, y)
-def rl() -> bytes: return p.recvline()
+if args.GDB:
+    p = elf.debug(gdbscript="\n".join(gdb_cmds))
+elif args.REMOTE:
+    p = remote("addr", 1337)
+else:
+    p = elf.process()
 
-def conn():
-    if args.GDB:
-        p = elf.debug(gdbscript="\n".join(gdb_cmds))
-    elif args.REMOTE:
-        p = remote("addr", 1337)
-    else:
-        p = elf.process()
-    return p
+def sla(x: bytes | str, y: bytes | str):
+    p.sendlineafter(x, y)
 
-def main():
-    p = conn()
+def rl() -> bytes:
+    return p.recvline()
 
-    # good luck buddy
-
-    p.interactive()
+# good luck buddy
 
 
-if __name__ == "__main__":
-    main()
+
+p.interactive()
