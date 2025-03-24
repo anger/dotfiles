8f257fd78393e6a5a7565e241e8aacd9180b4067
jax <48845671+anger@users.noreply.github.com>
2025-03-23 21:14:08 -0400

    added more one-liners

--- .config/pwn-template.py
+++ .config/pwn-template.py
@@ -11,7 +11,12 @@ context.log_level = "DEBUG"
 gdb_cmds = ["c"]
 
 def sla(x : bytes | str, y : bytes | str): p.sendlineafter(x, y)
+def sa(x: bytes | str, y: bytes | str): p.sendafter(x, y)
+def sl(x: bytes | str): p.sendline(x)
+def s(x: bytes | str): p.send(x)
 def rl() -> bytes: return p.recvline()
+def ru(x: bytes | str) -> bytes: return p.recvuntil(x)
+def rv(n: int) -> bytes: return p.recv(n)
 
 if args.GDB:
     p = elf.debug(gdbscript="\n".join(gdb_cmds))
